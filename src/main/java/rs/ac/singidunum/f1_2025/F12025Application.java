33722adbe4d675326fc87d952e68b82d14a7dcd6
redzyc <redzyc05@gmail.com>
2025-05-19 15:37:16 +0200

    Generated the project

---
+++ src/main/java/rs/ac/singidunum/f1_2025/F12025Application.java
@@ -0,0 +1,13 @@
+package rs.ac.singidunum.f1_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F12025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F12025Application.class, args);
+	}
+
+}
---
+++ src/test/java/rs/ac/singidunum/f1_2025/F12025ApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.singidunum.f1_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F12025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
